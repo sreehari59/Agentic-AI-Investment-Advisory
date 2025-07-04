25bfbb6176943ee941630f1c0e3bddf0e0e0cdba
Sreehari <theonlysreehari@gmail.com>
2025-07-04 11:10:20 +0200

    adding agent mapping function in helper.py

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -11,3 +11,18 @@ def get_agent_name(agent_list):
          "Valuation Analyst" :  "valuation_analyst"
     }
     return [agent_mapper.get(agent) for agent in agent_list if agent in agent_mapper]
+
+
+def agent_mapper(agent_name):
+    agent_mapper = {
+         "ben_graham_agent" : "Ben Graham",
+         "bill_ackman_agent": "Bill Ackman",
+         "cathie_wood_agent": "Cathie Wood",
+         "charlie_munger_agent": "Charlie Munger",
+         "warren_buffett_agent": "Warren Buffett",
+         "technical_analyst_agent": "Technical Analyst",
+         "fundamentals_agent": "Fundamentals Analyst",
+         "sentiment_agent": "Sentiment Analyst",
+         "valuation_agent": "Valuation Analyst"
+    }
+    return agent_mapper.get(agent_name, None)
